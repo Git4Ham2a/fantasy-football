709165ad2f36dd9b173096247c65a03fbc539cc1
hamza25540 <111743185+hamza25540@users.noreply.github.com>
2022-10-15 15:04:44 +0100

    Update models.py

--- application/models.py
+++ application/models.py
@@ -1,10 +1,9 @@
-#How your databases is going to look
-
+# How your databases are going to look. 
 from application import db 
 
 class Lists(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    tasks = db.Column(db.String())
+    lid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
 
 class Todos(db.Model):
     tid = db.Column(db.Integer, primary_key=True)
