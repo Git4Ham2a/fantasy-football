52c9e8dd14c73f35f82d321cefaec3fa75e0c8a0
hamza25540 <111743185+hamza25540@users.noreply.github.com>
2022-10-16 14:43:47 +0100

    Update forms.py

--- application/forms.py
+++ application/forms.py
@@ -1,12 +1,13 @@
-#Links database with roots by creating input forms for users
+# Links the database with your routes, by creating input forms. 
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
+from wtforms import StringField, SubmitField, SelectField, IntegerField
 
-from application.models import Todos, Lists
+from application.models import Todos, Lists 
 
-class TodoForm (FlaskForm):
+class TodoForm(FlaskForm):
     tasks = StringField("Task")
+    fk_lid = IntegerField("Lists ID")
     submit = SubmitField("Submit")
 
 class ListForm(FlaskForm):
