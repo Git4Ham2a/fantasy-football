17c3d715f196cf09aeca05c4e466624553f5953f
hamza25540 <111743185+hamza25540@users.noreply.github.com>
2022-10-19 02:04:14 +0100

    Update routes.py

--- application/routes.py
+++ application/routes.py
@@ -2,16 +2,16 @@
 from asyncio import Task
 from flask import render_template, url_for, redirect, request 
 from application import app, db 
-from application.models import Todos, Lists
-from application.forms import TodoForm, ListForm
+from application.models import Players, Teams
+from application.forms import PlayerForm, TeamForm
 
 #READ BOTH DATABASES
 #Location of this functionality: ip_address:5000/
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    lists = Lists.query.all()
-    todos = Todos.query.all()
-    return render_template('index.html', title="To do List", todos=todos, lists=lists)
+    teams = Teams.query.all()
+    players = Players.query.all()
+    return render_template('index.html', title="Fantasy Football", teams=teams, players=players)
 
 # CREATE list items 
 @app.route('/addlist', methods=['POST', 'GET'])
