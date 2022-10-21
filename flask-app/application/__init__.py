72bd9bde5098e1affb540bffd1b544abbf6b3c34
Ubuntu <azureuser@docker-practice.5e3jxxfk4aiubp2ispvu0mqkxg.bx.internal.cloudapp.net>
2022-10-21 14:22:48 +0000

    update

--- flask-app/application/__init__.py
+++ flask-app/application/__init__.py
@@ -1,14 +1,10 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
-
 app = Flask(__name__, template_folder="templates")
-
 # Sets up your database. We are using SQLAlchemy's sqlite. 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@mysql:3306/flask-db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'Secrets'
-
 db = SQLAlchemy(app)
-
 # imports the routes.py from application. 
 from application import routes
